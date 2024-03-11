8806d3fc450c8a70f84bb4383150d9a494376e4a
Iván Luna <133716241+imprvhub@users.noreply.github.com>
2024-03-11 17:50:32 -0300

    feat: mysql migration to postgres 'supabase'

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ import os
 from functools import wraps
 from io import BytesIO
 import string 
-import pymysql
+import psycopg2
 import random
 from pytz import timezone 
 import requests
@@ -19,19 +19,29 @@ app = Flask(__name__)
 domain_url = "https://apod-nasa-viewer.vercel.app"
 hashids_salt = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
 hashids = Hashids(salt=hashids_salt, min_length=4)
-connection = pymysql.connect(
-        host=os.getenv("DATABASE_HOST"),
-        user=os.getenv("DATABASE_USERNAME"),
-        passwd=os.getenv("DATABASE_PASSWORD"),
-        db=os.getenv("DATABASE"),
-        ssl={"ssl_accept": "strict"}
-    )
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_SSLMODE = os.getenv("DB_SSLMODE")
 
-with connection.cursor() as cursor:
-        cursor.execute("""
+connection = psycopg2.connect(
+    host=DB_HOST,
+    port=DB_PORT,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    dbname=DB_NAME,
+    sslmode=DB_SSLMODE
+)
+
+connection.autocommit = True
+cursor = connection.cursor()
+
+cursor.execute("""
             CREATE TABLE IF NOT EXISTS urls (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                original_url VARCHAR(255) NOT NULL,
+                id INT PRIMARY KEY,
+                original_url VARCHAR(2048) NOT NULL,
                 short_url VARCHAR(255) NOT NULL
             )
         """)
@@ -228,3 +238,4 @@ def user_agreements():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
